417aac5788d505a4d77776ca3a7bfe14a665556f
Csanyi Bence <csanyibence@gmail.com>
2021-08-23 16:20:26 +0200

    finally its operating TODO, cout damage build-up

--- OrcCreator/OrcCreator.cpp
+++ OrcCreator/OrcCreator.cpp
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <algorithm>
+#include <string>
 #include "orc.h"
 
 void WelcomeText();
@@ -13,10 +14,13 @@ void ShowOptions();
 
 // TODO
 void CreateOrc(std::string name, int strength, int endurance, int intelligence, int charisma);
-void ListOrcs(Orc array_orcs, int number_orcs);
+void ListOrcs(int number_orcs);
 
 int main()
 {
+    std::string random_name1 = "Uglukk";
+    std::string random_name2 = "Abzlug";
+
     int number_orcs = 0;
     std::string orc_name;
     int orc_strength;
@@ -25,7 +29,11 @@ int main()
     int orc_intelligence;
     int orc_charisma;
     
-    char user_choice;
+    std::string user_choice;
+    char real_choice;
+
+    int damage = 0;
+    srand(time(0));
 
     std::vector<Orc> array_orcs;
 
@@ -35,9 +43,10 @@ int main()
         ShowOptions();
         std::cout << "User choice:\t\t";
         std::cin >> user_choice;
+        real_choice = user_choice[0];
         std::cout << "\n";
 
-        if (user_choice == 'a') // Create orc
+        if (real_choice == 'a') // Create orc
         {
             std::cout << "Give name of the orc: ";
             std::cin >> orc_name;
@@ -71,7 +80,29 @@ int main()
             system("CLS");
             continue;
 
-        } if (user_choice == 'b')   // List orcs
+        } if (real_choice == 'b')   // Create 2 random orcs
+        {
+            // 1
+            Orc random_name1(random_name1, (rand()%15 + 6), (rand()%15 + 6), (rand()%15 + 6));
+            random_name1.SetIntelligence(rand()%15 + 6);
+            random_name1.SetCharisma(rand() % 15 + 6);
+            number_orcs++;
+            array_orcs.push_back(random_name1);
+
+            // 2
+            Orc random_name2(random_name2, (rand() % 15 + 6), (rand() % 15 + 6), (rand() % 15 + 6));
+            random_name2.SetIntelligence(rand() % 15 + 6);
+            random_name2.SetCharisma(rand() % 15 + 6);
+            number_orcs++;
+            array_orcs.push_back(random_name2);
+
+            std::cout << "Two random orcs have been created!\n\n";
+
+            system("pause");
+            system("CLS");
+            continue;
+
+        } if (real_choice == 'c')   // List orcs
         {
             std::cout << "Number of orcs: " << number_orcs << ".\n";
 
@@ -82,18 +113,60 @@ int main()
                 std::cout << "Dexterity:\t\t" << array_orcs[i].its_dexterity << "\n";
                 std::cout << "Endurance:\t\t" << array_orcs[i].its_endurance << "\n";
                 std::cout << "Intellect:\t\t" << array_orcs[i].GetIntelligence() << "\n";
-                std::cout << "Charisma:\t\t" << array_orcs[i].GetCharisma() << "\n\n";
+                std::cout << "Charisma:\t\t" << array_orcs[i].GetCharisma() << "\n";
+                std::cout << "Hit points:\t\t[" << array_orcs[i].its_health << "/" << array_orcs[i].its_health << "]\n\n";
             }
 
             system("pause");
             system("CLS");
             continue;
 
-        } if (user_choice == 'c')   // let them fight!
+        } if (real_choice == 'd')   // let them fight!
         {
-            continue;
+            if (number_orcs < 2)
+            {
+                std::cout << "There are too few orcs, please create another!\n\n";
+                system("pause");
+                system("CLS");
+                continue;
+            }
+            else
+            {
+                std::cout << "\tLET THE FIGHT BEGIN!\n\n";
+                std::cout << array_orcs[0].its_name << " VS " << array_orcs[1].its_name << "\n\n";
 
-        } if (user_choice == 'x' or user_choice != 'X')   // exit
+                do
+                {
+                    // 1
+                    std::cout << array_orcs[0].its_name << " is attacking. " << array_orcs[0].its_name << " is swinging his sword, doing ";
+                    damage = rand()%6 + 1 + (std::max(10, array_orcs[0].its_strength) - 10);
+                    std::cout << damage << " damage!\n";
+                    array_orcs[1].its_health -= damage;
+                    std::cout << array_orcs[1].its_name << "'s health is " << array_orcs[1].its_health << ".\n";
+
+                    if (array_orcs[1].its_health < 1) {
+                        std::cout << array_orcs[0].its_name << " has slain " << array_orcs[1].its_name  << "!\n";
+                        break;
+                    }
+
+                    // 2
+                    std::cout << array_orcs[1].its_name << " is attacking. " << array_orcs[1].its_name << " is swinging his sword, doing ";
+                    damage = rand() % 6 + 1 + (std::max(10, array_orcs[1].its_strength) - 10);
+                    std::cout << damage << " damage.\n";
+                    array_orcs[0].its_health -= damage;
+                    std::cout << array_orcs[0].its_name << "'s health is " << array_orcs[0].its_health << ".\n";
+                    if (array_orcs[0].its_health < 1) {
+                        std::cout << array_orcs[1].its_name << " has slain " << array_orcs[0].its_name << "!\n";
+                        
+                        break;
+                    }
+                } while (array_orcs[0].its_health > 0 && array_orcs[1].its_health > 0);
+
+                system("pause");
+                system("CLS");
+                continue;
+            }
+        } if (real_choice == 'x' or real_choice != 'X')   // exit
         {
             std::cout << "Exiting application...\n";
             break;
@@ -104,7 +177,7 @@ int main()
             std::cout << "\nWrong character. Please give another!\n";
             continue;
         }
-    } while (user_choice != 'x' && user_choice != 'X');
+    } while (real_choice != 'x' && real_choice != 'X');
     
     return 0;
 }
@@ -119,8 +192,9 @@ void WelcomeText() {
 void ShowOptions() {
     std::cout << "Choose from the options below:\n\n";
     std::cout << "Create new orc\t\t - a\n";
-    std::cout << "List existing orcs\t\ - b\n";
-    std::cout << "Let them fight!\t\t - c\n";
+    std::cout << "Create 2 randoms\t\ - b\n";
+    std::cout << "List existing orcs\t - c\n";
+    std::cout << "Let them fight!\t\t - d\n";
     std::cout << "Exit\t\t\t - x\n";
 }
 
@@ -128,7 +202,7 @@ void CreateOrc(std::string name, int strength, int endurance, int intelligence, 
 
 }
 
-void ListOrcs(Orc array_orcs, int number_orcs) {
+void ListOrcs(int number_orcs) {
 
 }
 
